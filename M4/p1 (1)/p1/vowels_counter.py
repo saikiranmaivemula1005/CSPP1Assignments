14310b97497c4f6fecfbc3eb8fed177e6dd5d1af
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-02 14:15:35 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- M4/p1 (1)/p1/vowels_counter.py
+++ M4/p1 (1)/p1/vowels_counter.py
@@ -9,7 +9,7 @@ def main():
 	# the input string is in s
 	# remove pass and start your code here
 	c=0
-	for letter in range s:
+	for letter in range (s):
                 if letter == 'a' or 'i' or 'e' or 'o' or 'u':
                         c +=1
         print (c)
