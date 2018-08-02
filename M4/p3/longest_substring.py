cb9dfcd8c1058ba328c641fd88eb809c6b317056
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-02 17:31:52 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- M4/p3/longest_substring.py
+++ M4/p3/longest_substring.py
@@ -1,22 +1,22 @@
 """kkk"""
 def main():
 	"""kk"""
-	str_ = input()
-	check_ = 0
-	mi_ = 0
-	co_ = 0
-	# the input string is in s
-	# remove pass and start your code here
-	while i < 1:
-		if str_[i] < str_[i+1]:
-			check_ = check_+1
-		else:
-			check_ = 0
-		if check_ > co_:
-			co_ = check_
-			mi_ = i
-		else:
-			i = i+1
-	print(substring(str_, mi_+1-co_, co_+1))
+    str_ = input()
+    check_ = 0
+    mi_ = 0
+    co_ = 0
+    # the input string is in s
+    # remove pass and start your code here
+    while i < 1:
+        if str_[i] < str_[i+1]:
+            check_ = check_+1
+        else:
+            check_ = 0
+        if check_ > co_:
+            co_ = check_
+            mi_ = i
+        else:
+            i = i+1
+    print(substring(str_, mi_+1-co_, co_+1))
 if __name__== "__main__":
-	main()
+    main()
