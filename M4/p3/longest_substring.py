c0d5b4ad62eaf2a9898337f30df885b0c3189ef7
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-02 17:29:50 +0530

    " -> 0 of 5 passed. pylint: -7.50/10 "

--- M4/p3/longest_substring.py
+++ M4/p3/longest_substring.py
@@ -1,17 +1,4 @@
 """kkk"""
-'''Assume s is a string of lower case characters.
-Write a program that prints the longest substring of s in which the letters occur in alphabetical order.
-For example, if s = 'azcbobobegghakl', then your program should print
-
-Longest substring in alphabetical order is: beggh
-
-In the case of ties, print the first substring.
-For example, if s = 'abcbcd', then your program should print
-
-Longest substring in alphabetical order is: abc
-
-Note: This problem may be challenging. We encourage you to work smart.
-''' hello.
 def main():
 	"""kk"""
 	str_ = input()
