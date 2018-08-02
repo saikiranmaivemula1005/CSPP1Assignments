c47a44b9430b7f89144435d0114c902c6e3fe7c5
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-02 14:36:15 +0530

    " -> 2 of 4 passed. pylint: -6.67/10 "

--- M4/p1 (1)/p1/vowels_counter.py
+++ M4/p1 (1)/p1/vowels_counter.py
@@ -2,14 +2,14 @@
 #Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
 #Number of vowels: 5
 def main():
-	S_t1 = input()
+	str_ = input()
 	# the input string is in s
 	# remove pass and start your code here
-	C_o1 = 0
-	for Lette_r1 in (S_t1): 
-	     if Lette_r1 == 'a' or 'i' or 'e' or 'o' or 'u':
-	         C_o1 += 1
-	print(C_o1)  
+	cou_ = 0
+	for letter_ in (str_): 
+	     if letter_ == 'a' or 'i' or 'e' or 'o' or 'u':
+	         cou_ += 1
+	print(cou_)  
                    
 if __name__== "__main__":
 	main()
