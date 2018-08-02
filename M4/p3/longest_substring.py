a805de78ea6f0e09e65b4a074030daa64de107b1
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-02 17:58:57 +0530

    " -> 5 of 5 passed. pylint: 8.46/10 "

--- M4/p3/longest_substring.py
+++ M4/p3/longest_substring.py
@@ -1,22 +1,16 @@
-"""kkk"""
+"""kk"""
 def main():
-	"""kk"""
+    """Count the occurence of the substring in a string"""
     str_ = input()
-    check_ = 0
-    mi_ = 0
-    co_ = 0
-    # the input string is in s
-    # remove pass and start your code here
-    while i < 1:
-        if str_[i] < str_[i+1]:
-            check_ = check_+1
-        else:
-            check_ = 0
-        if check_ > co_:
-            co_ = check_
-            mi_ = i
-        else:
-            i = i+1
-    print(substring(str_, mi_+1-co_, co_+1))
-if __name__== "__main__":
+    max_str = ''
+    for i in range(0, len(str_)):
+        sub_str = str_[i]
+        while i + 1 < len(str_) and str_[i] <= str_[i+1]:
+            i += 1
+            sub_str += str_[i]
+        if len(sub_str) > len(max_str):
+            max_str = sub_str
+    print(max_str)
+
+if __name__ == "__main__":
     main()
