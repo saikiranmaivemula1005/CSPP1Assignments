c324c7820c93c2015731118b4118b2ffba9a932d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-02 18:27:23 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- M4/p3/longest_substring.py
+++ M4/p3/longest_substring.py
@@ -1,16 +1,25 @@
 """kk"""
 def main():
-    """Count the occurence of the substring in a string"""
-    str_ = str(input())
-    max_str = ''
-    for i in range(0, len(str_)):
-        sub_str = str_[i]
-        while i + 1 < len(str_) and str_[i] <= str_[i+1]:
-            i += 1
-            sub_str += str_[i]
-        if len(sub_str) > len(max_str):
-            max_str = sub_str
-    print(max_str)
+    ''' gives longest_substring '''
+    str_ = input()
+    # the input string
+    tstr_ = ""
+    for i, item in enumerate(str_):
+        flag_ = 0
+        tstr2_ = ""
+        tt_ = item
+        ji_ = i
+        while flag_ != 1 and ji_ != len(str_):
+            if ord(tt_) <= ord(str_[ji_]):
+                tt_ = str_[ji_]
+                tstr2_ += str_[ji_]
+            else:
+                flag_ = 1
+            ji_ += 1
+
+        if len(tstr2_) > len(tstr_):
+            tstr_ = tstr2_
+    print(tstr_)
 
 if __name__ == "__main__":
     main()
