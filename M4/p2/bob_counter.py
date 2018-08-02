163b32529b00ca982b527caf343cb084fee38bf8
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-02 16:29:12 +0530

    " -> 0 of 5 passed. pylint: 1.67/10 "

--- M4/p2/bob_counter.py
+++ M4/p2/bob_counter.py
@@ -3,8 +3,8 @@ Number of times bob occurs is: 2'''
 def main():
 	# the input string is in s
 	# remove pass and start your code here
-	s_1 = input("enter a string")
-	s_2 = input("enter a sub string")
+	s_1 = input()
+	s_2 = input()
 	print(s_1.count(s_2))
 if __name__ == "__main__":
 	main()
