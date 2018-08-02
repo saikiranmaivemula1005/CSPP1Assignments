593393abc36b30e3ef6da288237475e2fe5865e2
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-02 17:02:59 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- M4/p2/bob_counter.py
+++ M4/p2/bob_counter.py
@@ -3,7 +3,11 @@ Number of times bob occurs is: 2'''
 def main():
     """geee"""
     s_1 = input()
-    s_2 = input()
-    print(s_1.count(s_2))
+    s_2 = 'bob'
+    co_ = 0
+    for i in range(len(s_1)-2):
+        if s_2 == s_1[i]+s_1[i+1]+s_1[i+2]:
+            co_ = co_+1
+    print(co_)
 if __name__ == "__main__":
     main()
