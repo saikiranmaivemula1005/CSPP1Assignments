a72a40ca5e2a83f57fa595c1e92356eee9fc4537
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-03 18:36:58 +0530

    " -> 3 of 3 passed. pylint: 5.71/10 "

--- M5/p2/square_root.py
+++ M5/p2/square_root.py
@@ -7,19 +7,18 @@
 # input: 49
 # output: 6.999999999999991
 def main():
-	"""kk"""
-    cube = int(input())
-    epsilon = 0.01
-    guess = 0.0
-    increment = 0.1
-    num_guesses = 0
-    while abs(guess**2 - cube) >= epsilon:
-        guess += increment
-        num_guesses += 1
-    if abs(guess**2 - cube) >= epsilon:
-        print('Failed on cube root of', cube)
-    else:
-        print(guess)
-
+    """kk"""
+cube = int(input())
+epsilon = 0.01
+guess = 0.0
+increment = 0.1
+num_guesses = 0
+while abs(guess**2 - cube) >= epsilon:
+    guess += increment
+    num_guesses += 1
+if abs(guess**2 - cube) >= epsilon:
+    print('Failed on cube root of', cube)
+else:
+    print(guess)
 if __name__== "__main__":
     main()
