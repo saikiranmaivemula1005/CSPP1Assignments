002c8efefc76980e94c1d25afad5b6a56634a7c9
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-03 17:33:34 +0530

    " -> 0 of 3 passed. pylint: 2.50/10 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -8,14 +8,14 @@ Step_ = 0.01
 Guesses_ = 0
 Low_ = 0.0
 High_ = inp_
-Ans_ = (High_ + Low_)/2
+Ans_ = (High_ + Low_)/2.0
 while abs(Ans_**2 - inp_) >= Step_:
     Guesses_ += 1
     if Ans_**2 < inp_:
         Low_ = Ans_
     else:
         High_ = Ans_
-    Ans_ = (High_ + Low_)/2
+    Ans_ = (High_ + Low_)/2.0
 print(str(inp_))
 if __name__== "__main__":
     main()
