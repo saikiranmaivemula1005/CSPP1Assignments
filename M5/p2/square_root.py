3080d194d565c2e58b9229cd7566fe41daf3c2c1
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-03 18:35:32 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M5/p2/square_root.py
+++ M5/p2/square_root.py
@@ -1,15 +1,13 @@
-# Write a python program to find the square root of the given number
+"""kk"""# Write a python program to find the square root of the given number
 # using approximation method
-
 # testcase 1
 # input: 25
 # output: 4.999999999999998
-
 # testcase 2
 # input: 49
 # output: 6.999999999999991
-
 def main():
+	"""kk"""
     cube = int(input())
     epsilon = 0.01
     guess = 0.0
@@ -24,4 +22,4 @@ def main():
         print(guess)
 
 if __name__== "__main__":
-	main()
+    main()
