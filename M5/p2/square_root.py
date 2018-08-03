447b5bab9b846ca9709b562bc827858c9b20727d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-03 18:30:34 +0530

    " -> 0 of 3 passed. pylint: 7.33/10 "

--- M5/p2/square_root.py
+++ M5/p2/square_root.py
@@ -10,15 +10,19 @@
 # output: 6.999999999999991
 
 def main():
-guess = 0.0
-increment = 0.01
-num_guesses = 0
-while abs(guess**2 - cube) >= epsilon and guess <= cube:
-    guess += increment
-    num_guesses += 1
-print('num_guesses =', num_guesses)
-if abs(guess**2 - cube) >= epsilon:
-else:
-    print(guess)
+    cube = int(input())
+    epsilon = 0.01
+    guess = 0.0
+    increment = 0.00001
+    num_guesses = 0
+    while abs(guess**2 - cube) >= epsilon:
+        guess += increment
+        num_guesses += 1
+    print('num_guesses =', num_guesses)
+    if abs(guess**2 - cube) >= epsilon:
+        print('Failed on cube root of', cube)
+    else:
+        print(guess, 'is close to the cube root of', cube)
+
 if __name__== "__main__":
 	main()
