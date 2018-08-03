27893470bb40d4f4c594605674ef0226fd4a00bc
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-03 16:52:33 +0530

    " -> 0 of 1 passed. pylint: 10.00/10 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -1,9 +1,23 @@
+"""kk"""
 # Write a python program to find the square root of the given number 
 # using Bi-section method
-
 def main():
-	s = input()
-	
-
+	"""kk"""
+inp_ = int(input())
+Step_ = 0.01
+Guesses_ = 0
+Low_ = 0.0
+High_ = inp_
+Ans_ = (High_ + Low_)/2
+while abs(Ans_**2 - inp_) >= Step_:
+    print('low = ' + str(Low_) + ' high = ' + str(High_) + ' ans = ' + str(Ans_))
+    Guesses_ += 1
+    if Ans_**2 < inp_:
+        Low_ = Ans_
+    else:
+    	High_ = Ans_
+    Ans_ = (High_ + Low_)/2
+print('Guesses_ = ' + str(Guesses_))
+print(str(Ans_) + ' is close to square root of ' + str(inp_))
 if __name__== "__main__":
 	main()
--- M5/p4/square_root_newtonrapson.py
+++ M5/p4/square_root_newtonrapson.py
@@ -1,9 +1,15 @@
-# Write a python program to find the square root of the number 
+"""kkk"""# Write a python program to find the square root of the number
 # using Newton-Rapson method
-
 def main():
-	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+    """kk"""
+STEP_ = 0.01
+Y_ = int(input())
+GUESS_ = Y_/2.0
+NOG_ = 0
+while abs(GUESS_*GUESS_ - Y_) >= STEP_:
+    NOG_ += 1
+    GUESS_ = GUESS_ - (((GUESS_**2) - Y_)/(2*GUESS_))
+print('numGuesses = ' + str(NOG_))
+print('Square root of ' + str(Y_) + ' is about ' + str(GUESS_))
+if __name__ == "__main__":
+    main()
