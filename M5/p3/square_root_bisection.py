39481f4f42efd27a825f51ee659dc4c10d3df83f
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-03 17:28:42 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M5/p3/square_root_bisection.py
+++ M5/p3/square_root_bisection.py
@@ -3,19 +3,19 @@
 # using Bi-section method
 def main():
 	"""kk"""
-inp_ = int(input())
-Step_ = 0.01
-Guesses_ = 0
-Low_ = 0.0
-High_ = inp_
-Ans_ = (High_ + Low_)/2
-while abs(Ans_**2 - inp_) >= Step_:
-        Guesses_ += 1
-    if Ans_**2 < inp_:
-        Low_ = Ans_
-    else:
-        High_ = Ans_
+    inp_ = int(input())
+    Step_ = 0.01
+    Guesses_ = 0
+    Low_ = 0.0
+    High_ = inp_
     Ans_ = (High_ + Low_)/2
-print(str(inp_))
+    while abs(Ans_**2 - inp_) >= Step_:
+            Guesses_ += 1
+        if Ans_**2 < inp_:
+            Low_ = Ans_
+        else:
+            High_ = Ans_
+        Ans_ = (High_ + Low_)/2
+    print(str(inp_))
 if __name__== "__main__":
-	main()
+    main()
