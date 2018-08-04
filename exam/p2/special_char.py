11c7f3cd14e705351c7796e453a7e6d5ff140007
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-04 12:04:34 +0530

    " -> 1 of 7 passed. pylint: 10.00/10 "

--- exam/p2/special_char.py
+++ exam/p2/special_char.py
@@ -9,7 +9,7 @@ def main():
     '''
     inp_ = input()
     i = 0
-    for i enumerate(len(inp_))
+    for i, char_ in enumerate(inp_):
         char_ = inp_[i]
         if char_ in('!' or '@' or '#' or '$' or '%' or '^' or '&' or '*'):
             print('')
