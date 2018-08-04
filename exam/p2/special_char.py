c2fe415cada0de37cd944314c73ed6aee3310b74
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-04 11:21:34 +0530

    " -> 1 of 7 passed. pylint: 10.00/10 "

--- exam/p2/special_char.py
+++ exam/p2/special_char.py
@@ -8,8 +8,8 @@ def main():
     Read string from the input, store it in variable str_input.
     '''
     inp_ = input()
-    for char in inp_:
-        if char in('!' or '@' or '#' or '$' or '%' or '^' or '&' or '*'):
+    for i in inp_:
+        if inp_[i] in('!' or '@' or '#' or '$' or '%' or '^' or '&' or '*'):
             print(" OK  ")
     print(inp_)
 if __name__ == "__main__":
