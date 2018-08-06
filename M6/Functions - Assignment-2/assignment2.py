759ac6e923f3e9f53d72ac50ef5b5a6c8d3e0d2d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-06 20:07:41 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- M6/Functions - Assignment-2/assignment2.py
+++ M6/Functions - Assignment-2/assignment2.py
@@ -1,46 +1,30 @@
-# Assignment-2 - Paying Debt off in a Year
-
-# Now write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months.
-# By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be
-# paid each month.
-
-# In this problem, we will not be dealing with a minimum monthly payment rate.
-
-# The following variables contain values as described below:
-# balance - the outstanding balance on the credit card
-# annualInterestRate - annual interest rate as a decimal
-
-# The program should print out one line: the lowest monthly payment that will pay off all debt in under 1 year, for example:
-# Lowest Payment: 180
-
-# Assume that the interest is compounded monthly according to the balance at the end of the month (after the payment for that month is
-# made).
-# The monthly payment must be a multiple of $10 and is the same for all months. Notice that it is possible for the balance to become
-# negative using this payment scheme, which is okay. A summary of the required math is found below:
-# Monthly interest rate = (Annual interest rate) / 12.0
-# Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
-
-
-def payingDebtOffInAYear(balance, annualInterestRate):
-    MIR = annualInterestRate / 12.0
-    MFMP = 10
-    while balance > 0:        
-        for i in range (1, 13):
-            MUB = balance - MFMP
-            UBEM = MUB + MIR * MUB
-            balance = UBEM
-        if balance != 0:
-            MFMP += 10
-        else:
-            return round (balance, 2)
-
+"""code"""
+def paying_debt(balance_1, annual_interest_rate):
+    """code"""
+    mfmp_ = 0
+    temp_bal = balance_1
+    year_count = 1
+    while balance_1 > 0:
+        mfmp_ += 10
+        balance_1 = temp_bal
+        i = 0
+        # print(mfmp_,year_count)
+        for i in range(1, 13):
+            m_intrest_rate = annual_interest_rate/12.0
+            monthly_unpaid_balance = balance_1 - mfmp_
+            balance_1 = monthly_unpaid_balance + m_intrest_rate*monthly_unpaid_balance
+            i = i+1
+            # balance_1 = updated_balance_each_month
+            # print (i,balance_1)
+        # if balance_1 <=0.5:
+        #     break
+        year_count += 1
+    return mfmp_
 def main():
+    """code"""
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print(payingDebtOffInAYear(data[0],data[1]))
-	
-if __name__== "__main__":
+    print("Lowest Payment:", paying_debt(data[0], data[1]))
+if __name__ == "__main__":
     main()
