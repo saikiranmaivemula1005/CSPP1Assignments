2c00768aaa9583d2d33a948bbcf35c6b8c0eccc8
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-06 18:01:50 +0530

    " -> 0 of 10 passed. pylint: 8.89/10 "

--- M6/Functions - Assignment-1/assignment1.py
+++ M6/Functions - Assignment-1/assignment1.py
@@ -2,6 +2,7 @@
 def payingdebtoff_inayear(balance_, annual_interest_rate_, monthly_payment_rate_):
     """kkk"""
     mir_ = annual_interest_rate_ / 12
+    nom_ = 1
     while nom_ > 12:
         mir_ = annual_interest_rate_ / 12
         mmp_ = monthly_payment_rate_ * balance_
@@ -15,6 +16,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Remaining balance:", payingdebtoff_inayear(data[0], data[1], data[2]))
+    print("Remaining balance:",payingdebtoff_inayear(data[0], data[1], data[2]))
 if __name__ == "__main__":
     main()
