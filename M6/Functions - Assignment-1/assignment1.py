ea5d9bab830af318543a4948372dccb489703e03
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-06 17:50:55 +0530

    " -> 10 of 10 passed. pylint: -6.25/10 "

--- M6/Functions - Assignment-1/assignment1.py
+++ M6/Functions - Assignment-1/assignment1.py
@@ -1,41 +1,33 @@
-# Functions | Assignment-1 - Paying Debt off in a Year
-
+""""kkk"""# Functions | Assignment-1 - Paying Debt off in a Year
 # Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the
 # credit card company each month.
-
 # The following variables contain values as described below:
 # balance - the outstanding balance on the credit card
 # annualInterestRate - annual interest rate as a decimal
 # monthlyPaymentRate - minimum monthly payment rate as a decimal
-
 # For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
 # balance. Be sure to print out no more than two decimal digits of accuracy - so print
-
 # Remaining balance: 813.41
 # instead of
 # Remaining balance: 813.4141998135
-
 # So your program only prints out one thing: the remaining balance at the end of the year in the format:
 # Remaining balance: 4784.0
-
 # A summary of the required math is found below:
 # Monthly interest rate= (Annual interest rate) / 12.0
 # Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
 # Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
 # Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
-
-def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
-    mir_ = annualInterestRate / 12
+def payingDebtOffInAYear(balance_, annualInterestRate_, monthlyPaymentRate_):
+    mir_ = annualInterestRate_ / 12
     for i in range (1, 13):
-    	mir_ = annualInterestRate / 12
-    	mmp_ = monthlyPaymentRate * balance
-    	mub_ = balance - mmp_
+    	mir_ = annualInterestRate_ / 12
+    	mmp_ = monthlyPaymentRate_ * balance_
+    	mub_ = balance_ - mmp_
     	ubem_ = mub_ + mir_ * mub_
-    	balance = ubem_
+    	balance_ = ubem_
     return round (ubem_, 2)
-    	
 def main():
+	"""kk"""
 	data = input()
 	data = data.split(' ')
 	data = list(map(float, data))
