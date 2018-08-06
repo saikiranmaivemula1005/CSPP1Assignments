ed77c8a523b0b4fc631bdb1e220ae1a835cc56bf
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-06 17:53:06 +0530

    " -> 10 of 10 passed. pylint: -6.25/10 "

--- M6/Functions - Assignment-1/assignment1.py
+++ M6/Functions - Assignment-1/assignment1.py
@@ -31,7 +31,7 @@ def main():
 	data = input()
 	data = data.split(' ')
 	data = list(map(float, data))
-	print("Remaining balance:",payingDebtOffInAYear(data[0],data[1],data[2]))
+	print("Remaining balance:",payingDebtOff_InAYear(data[0],data[1],data[2]))
 if __name__ == "__main__":
 	main()
 
