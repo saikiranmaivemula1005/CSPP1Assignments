74722ca48dad07584e08b0d757b086e0cacec014
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-07 16:23:38 +0530

    " -> 10 of 10 passed. pylint: 8.18/10 "

--- M8/p2/assignment2.py
+++ M8/p2/assignment2.py
@@ -4,12 +4,12 @@ def sum_of_digits(num_):
     """kk"""
     temp_ = num_
     while temp_ > 0:
-    	temp_1 = temp_ % 10
-    	return temp_1 + sum_of_digits(temp_ // 10)
-    return 0   
+        temp_1 = temp_ % 10
+        return temp_1 + sum_of_digits(temp_ // 10)
+    return 0 
 def main():
     """kk"""
     inp_ = input()
-    print(sum_of_digits(int(inp_)))  
+    print(sum_of_digits(int(inp_)))
 if __name__ == "__main__":
     main()
