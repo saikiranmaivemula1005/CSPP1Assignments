982bedf84f3cd90497f091b58f24fcb8c6a1fef3
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-07 15:11:36 +0530

    " -> 10 of 10 passed. pylint: 6.67/10 "

--- M8/p1/assignment1.py
+++ M8/p1/assignment1.py
@@ -1,26 +1,17 @@
 """kk"""
 # Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
-
 # This function takes in one number and returns one number.
-
-
 def factorial(num_):
     '''
     n is positive Integer
-
     returns: a positive integer, the factorial of n.
-    '''
-    
+    '''   
     if num_ in (0, 1):
         return 1
     return num_ * factorial(num_-1)
-    
-
-
 def main():
     """kk"""
     inp_ = input()
-    print(factorial(int(inp_)))    
+    print(factorial(int(inp_)))
 if __name__== "__main__":
     main()
