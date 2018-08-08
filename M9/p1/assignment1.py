f47d1c35514726180c20bf660f95c0c6b87dc0d6
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-08 16:51:25 +0530

    " -> 3 of 9 passed. pylint: 6.67/10 "

--- M9/p1/assignment1.py
+++ M9/p1/assignment1.py
@@ -15,7 +15,7 @@ def is_word_guessed(secret_word, letters_guessed):
       False otherwise
     '''
     c = 0
-    for i in range(list(secret_word)):
+    for i in range(len(secret_word)):
         if secret_word[i]  == letters_guessed[i]:
             c = c + 1
         if c == len(secret_word):
