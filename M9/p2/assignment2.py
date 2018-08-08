caf8fb40bbd942daf00e6d8468e56ddd14e8ccbf
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-08 17:57:02 +0530

    " -> 0 of 9 passed. pylint: 8.26/10 "

--- M9/p2/assignment2.py
+++ M9/p2/assignment2.py
@@ -20,7 +20,7 @@ def get_guessed_word(secret_word, letters_guessed):
             if ite1_ == ite_:
                 x3 = x3 + x1_
             else:
-                x2 = x3 + '_'
+                x3 = x3 + '_'
         return x3
 
 def main():
