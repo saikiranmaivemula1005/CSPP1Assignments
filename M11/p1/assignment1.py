745bc317903225a6363f93363f1bc5e4c2c13b5d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-10 16:36:16 +0530

    " -> 4 of 9 passed. pylint: 3.64/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -28,7 +28,7 @@ def get_word_score(word, n):
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     length = len(word)
-    if n < length:
+    if n <= length:
         for i in word:
             sum = sum + fSCRABBLE_LETTER_VALUES[i]
     #print(sum) 
