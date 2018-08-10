79cf65139799ef2bd7e2279f0434ad6f156ac3a0
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-10 18:33:22 +0530

    " -> 3 of 3 passed. pylint: -1.18/10 "

--- M11/p3/assignment3.py
+++ M11/p3/assignment3.py
@@ -1,13 +1,7 @@
 # Assignment-3
 '''
-At this point, we have written code to generate a random hand and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
-
-Testing: Make sure the test_isValidWord tests pass. In addition, you will want to test your implementation by calling it multiple times on the same hand - what should the correct behavior be? Additionally, the empty string ('') is not a valid word - if you code this function correctly, you shouldn't need an additional check for this condition.
-
-Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
 '''
-
-def isValidWord(word, hand, wordList):
+def isValidWord(word_, hand_, word_list):
     """
     Returns True if word is in the wordList and is entirely
     composed of letters in the hand. Otherwise, returns False.
@@ -19,15 +13,10 @@ def isValidWord(word, hand, wordList):
     wordList: list of lowercase strings
     """
     # TO DO ... <-- Remove this comment when you code this function
-    for i in word:
-        if i not in hand.keys(): 
+    for ite_ in word_:
+        if ite_ not in hand_.keys(): 
             return False
-    return bool(word in wordList)
-
-
-    
-
-
+    return bool(word_ in word_list)
 def main():
     word=input()
     n=int(input())
@@ -37,9 +26,7 @@ def main():
         l=data.split()
         adict[l[0]]=int(l[1])
     l2=input().split()
-    print(isValidWord(word,adict,l2))
-        
-
-
-if __name__== "__main__":
+    print(isValidWord(word,adict,l2)) 
+if __name__ == "__main__":
     main()
+    
