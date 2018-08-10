d0035c160311eb0f6cbfe73fb106e085b54dbf89
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-10 17:47:35 +0530

    " -> 6 of 6 passed. pylint: 4.38/10 "

--- M11/p2/assignment2.py
+++ M11/p2/assignment2.py
@@ -1,10 +1,10 @@
 """kkk"""
-def updateHand(hand, word):
+def update_hand(hand_, word_):
     """kkk"""
-    for i in word:
-        if i in hand.keys():
-            hand[i] -= 1
-    return hand
+    for ite_ in word_:
+        if ite_ in hand_.keys():
+            hand_[ite_] -= 1
+    return hand_
 def main():
     """kk"""
     n = input()
@@ -14,6 +14,6 @@ def main():
         l=data.split()
         adict[l[0]]=int(l[1])
     data1=input()
-    print(updateHand(adict, data1))
+    print(update_hand(adict, data1))
 if __name__ == "__main__":
 	main()
