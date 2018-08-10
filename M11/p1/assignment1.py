bd6af1634721313d831530e841568d3f717cd9c4
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-10 16:42:35 +0530

    " -> 9 of 9 passed. pylint: 5.79/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -19,16 +19,16 @@ def get_word_score(word, n):
     returns: int >= 0
     """
     sum = 0
-    fSCRABBLE_LETTER_VALUES = {
+    scramble_letters  = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
     length = len(word)
     if length <= n:
         for i in word:
-            if i in fSCRABBLE_LETTER_VALUES.keys():
-                sum = sum + fSCRABBLE_LETTER_VALUES[i]
-    #print(sum) 
+            if i in scramble_letters.keys():
+                sum = sum + scramble_letters[i]
+    #print(sum)
     #print(di1)
         sum = sum * length
         if n == length:
@@ -37,7 +37,7 @@ def get_word_score(word, n):
     else:
         return'invalid'
     # TO DO ... <-- Remove this comment when you code this function
-    
+
 
 
 def main():
