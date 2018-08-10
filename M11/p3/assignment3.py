81e626fcac0c56d051791a5b3ae60ed563287d99
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-10 18:37:32 +0530

    " -> 3 of 3 passed. pylint: 9.44/10 "

--- M11/p3/assignment3.py
+++ M11/p3/assignment3.py
@@ -3,16 +3,14 @@ def is_valid_word(word_, hand_, word_list):
     """
     Returns True if word is in the wordList and is entirely
     composed of letters in the hand. Otherwise, returns False.
-
     Does not mutate hand or wordList.
-   
     word: string
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
     """
     # TO DO ... <-- Remove this comment when you code this function
     for ite_ in word_:
-        if ite_ not in hand_.keys(): 
+        if ite_ not in hand_.keys():
             return False
     return bool(word_ in word_list)
 def main():
@@ -26,6 +24,6 @@ def main():
         adict[le_[0]] = int(le_[1])
         i = i + 1
     l2_ = input().split()
-    print(is_valid_word(word, adict,l2_)) 
+    print(is_valid_word(word, adict,l2_))
 if __name__ == "__main__":
     main()
