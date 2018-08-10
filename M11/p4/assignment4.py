c36b7433a0f78bb784f613238297de2798cf58ea
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-10 18:51:09 +0530

    " -> 0 of 5 passed. pylint: -4.67/10 "

---
+++ M11/p4/assignment4.py
@@ -0,0 +1,26 @@
+"""kkk"""
+def calculate_handlen(hand_):
+    """ 
+    Returns the length (number of letters) in the current hand.
+    
+    hand: dictionary (string int)
+    returns: integer
+    """
+    # TO DO... <-- Remove this comment when you code this function
+    sum_ = 0
+    for i in hand_:
+    	sum_ = sum_ + hand_[i]
+    return sum_ 
+    
+
+def main():
+	"""kk"""
+	num_ = input()
+	adict = {}
+	for i in range(int(n)):
+		data = input()
+		le_ = data.split()
+		adict[le_[0]] = int(le_[1])
+	print(calculate_handlen(adict))	
+if __name__ == "__main__":
+	main()
