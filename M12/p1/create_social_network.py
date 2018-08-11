aa1da6615302f53f74058b75f66d6053252caddf
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-11 10:55:30 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -34,14 +34,14 @@ def create_social_network(data):
 
     # remove the pass below and start writing your code
     d1 = {}
-    data = data.split('follows ,')
+    data = data.split('\n')
+    data = data.split('follows')
     for i in data:
-        if i.split('follows')[0] not in d1:
-            d1[i.split('follows')[0]] = list(i.split('follows')[1])
-        else:
-            d1[i.split('follows')[0]].append(i.split('follows')[1])
-        return (d1)
-
+    if i.split('follows')[0] not in d1:
+        d1[i.split('follows')[0]] = list(i.split('follows')[1])
+    else:
+        d1[i.split('follows')[0]].append(i.split('follows')[1])
+    return(d1)
 
 def main():
     '''
