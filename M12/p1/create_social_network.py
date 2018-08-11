16e5b83e05b339fb13efcd867077b15664bd2397
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-11 11:40:56 +0530

    " -> 0 of 3 passed. pylint: 8.10/10 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -39,7 +39,7 @@ def create_social_network(data):
     data = data.split('\n')
     print(data)
     for i in data:
-        print(i)
+        print(i.split(' follows '))
         if i.split(' follows ')[0] not in d1:
             d1[i.split(' follows ')[0]] = str((i.split(' follows ')[1]).split(","))
         else:
