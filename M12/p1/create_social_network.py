a5ef3770dd1c4b25aa84a90b98701212582e6fbf
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-11 12:54:52 +0530

    " -> 0 of 3 passed. pylint: 9.50/10 "

--- M12/p1/create_social_network.py
+++ M12/p1/create_social_network.py
@@ -36,9 +36,9 @@ def create_social_network(data):
     x1_ = data.split('\n')
 # print(x)
     dic_ = {}
-    
+    data = data.split(' follows ')
     # print(data)
-    for ite_ in x1_:
+    for ite_ in data:
         ite_ = ite_.split(' follows ')
         # print(i[0],i[1])
         if ite_[0] not in dic_:
