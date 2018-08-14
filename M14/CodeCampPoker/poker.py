e26af4a9ad280d601287e6b4080849fded4701c3
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-14 09:48:38 +0530

    " -> 1 of 3 passed. pylint: 4.81/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -14,11 +14,10 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    for i in hand:
-        if hand[i][0] > hand[i+1][0]:
-            return True
-        else:
-            return False
+    if hand == hand.sort():
+        return True
+    else:
+        return False
 
 def is_flush(hand):
     '''
