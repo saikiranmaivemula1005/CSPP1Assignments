eee0a9c53c76408db3332cc2f28762cef0dc6918
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-14 09:55:54 +0530

    " -> 0 of 3 passed. pylint: 5.17/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -93,8 +93,10 @@ if __name__ == "__main__":
     # iterate through the test cases to set up hands list
     HANDS = []
     for x in range(COUNT):
+        print(x)
         line = input()
         ha = line.split(" ")
         HANDS.append(ha)
+        print(HANDS)
     # test the poker function to see how it works
-    # print(' '.join(poker(HANDS)))
+    print(' '.join(poker(HANDS)))
