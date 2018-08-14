d9efabb9bdac3519afb1b13c06e1c7d897d75b2a
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-14 13:05:51 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     COUNT = int(input())
     # iterate through the test cases to set up hands list
     HANDS = []
-    for x1_ in range(COUNT):
+    for x in range(COUNT):
         # print(x1_)
         line = input()
         ha = line.split(" ")
