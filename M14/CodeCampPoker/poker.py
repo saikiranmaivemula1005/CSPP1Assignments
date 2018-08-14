75ac4631bf0ede232b4c3fba70af1e4e2f68af7b
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-14 10:20:04 +0530

    " -> 0 of 3 passed. pylint: 6.43/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -15,7 +15,7 @@ def is_straight(hand):
         Write the code for it and return True if it is a straight else return False
     '''
     for i in hand:
-        if hand[i+1][i][0] < hand [i][i][0]:
+        if int(hand[i+1][i][0]) < int(hand [i][i][0]):
             return True
         else:
             return False
