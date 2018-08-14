1fa9e7e3df54fae9d4976ce64096c7a273f5ebd2
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-14 12:58:28 +0530

    " -> 0 of 3 passed. pylint: 5.68/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -13,19 +13,19 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    x = []
-    s = {'T':10, 'J':11, 'K':13, 'Q':12, 'A':14}
+    x1_ = []
+    s1_ = {'T':10, 'J':11, 'K':13, 'Q':12, 'A':14}
     for i in hand: 
         # print(i)
-        if i[0] in s.keys():
-            z = s[i[0]]
+        if i[0] in s1_.keys():
+            z = s1_[i[0]]
         else:
             z = int(i[0])
-        x.append(z)
-    x.sort()
+        x1_.append(z)
+    x1_.sort()
     for j in range(4):
-        t = x[j]+1
-        if t != x[j+1]:
+        t = x1_[j]+1
+        if t != x1_[j+1]:
             return False
     return True           
 def is_flush(hand):
@@ -44,14 +44,14 @@ def is_flush(hand):
 def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
-        return a value that max can use to identify the best hand.
-        As this function is complex we will progressively develop it.
+        return a value that max1_ can use to identify the best hand.
+        As this function is complex1_ we will progressively develop it.
         The first version should identify if the given hand is a straight
         or a flush or a straight flush.
     '''
     # By now you should have seen the way a card is represented.
     # If you haven't then go the main or poker function and print the hands
-    # Each card is coded as a 2 character string. Example Kind of Hearts is KH
+    # Each card is coded as a 2 character string. Ex1_ample Kind of Hearts is KH
     # First character for face value 2,3,4,5,6,7,8,9,T,J,Q,K,A
     # Second character for the suit S (Spade), H (Heart), D (Diamond), C (Clubs)
     # What would be the logic to determine if a hand is a straight or flush?
@@ -62,7 +62,7 @@ def hand_rank(hand):
     # the second best would be a flush with the return value 2
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
-    # max in poker function uses these return values to select the best hand
+    # max1_ in poker function uses these return values to select the best hand
     if is_straight(hand) == True and is_flush(hand) == True:
         return 3
     elif is_flush(hand) == True:
@@ -82,20 +82,20 @@ def poker(hands):
         Output: Return the winning poker hand
     '''
     # the line below may be new to you
-    # max function is provided by python library
+    # max1_ function is provided by python library
     # learn how it works, in particular the key argument, from the link
-    # https://www.programiz.com/python-programming/methods/built-in/max
-    # hand_rank is a function passed to max
+    # https://www.programiz.com/python-programming/methods/built-in/max1_
+    # hand_rank is a function passed to max1_
     # hand_rank takes a hand and returns its rank
-    # max uses the rank returned by hand_rank and returns the best hand
-    return max(hands, key=hand_rank)
+    # max1_ uses the rank returned by hand_rank and returns the best hand
+    return max1_(hands, key=hand_rank)
 if __name__ == "__main__":
     # read the number of test cases
     COUNT = int(input())
     # iterate through the test cases to set up hands list
     HANDS = []
-    for x in range(COUNT):
-        # print(x)
+    for x1_ in range(COUNT):
+        # print(x1_)
         line = input()
         ha = line.split(" ")
         HANDS.append(ha)
