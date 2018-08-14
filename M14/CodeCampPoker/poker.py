adb465875f1a68852db32951ee8e7711b75502da
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-14 12:23:09 +0530

    " -> 1 of 3 passed. pylint: 6.22/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -24,7 +24,7 @@ def is_straight(hand):
             z = int(i[0])
         x.append(z)
     x.sort()
-    for j in range(len(x)+1):
+    for j in range(4):
         t = x[j]+1
         if t != x[j+1]:
             return False
