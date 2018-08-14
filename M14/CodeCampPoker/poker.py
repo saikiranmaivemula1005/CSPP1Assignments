1548f94c5474aeef7ef19f1af015495001a46b84
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-14 09:54:37 +0530

    " -> 0 of 3 passed. pylint: 4.62/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -97,4 +97,4 @@ if __name__ == "__main__":
         ha = line.split(" ")
         HANDS.append(ha)
     # test the poker function to see how it works
-    print(' '.join(poker(HANDS)))
+    # print(' '.join(poker(HANDS)))
