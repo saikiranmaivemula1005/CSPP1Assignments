9739decd1e485166f967a08e17efbf4c686e95fa
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-17 12:35:19 +0530

    " -> 0 of 3 passed. pylint: 6.56/10 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -21,31 +21,21 @@ def similarity(dict1, dict2):
         if i in stopwords:
             doc_2.remove(i)
     # print(doc_2)
-    fre1 = []
-    fre2 = []
-    for i in doc_1:
-        fre1.append(doc_1.count(i))
-    # print(fre1)
-    for i in doc_2:
-        fre2.append(doc_2.count(i))
-    # print(fre2)
+    # fre1 = []
+    # fre2 = []
+    # for i in doc_1:
+    #     fre1.append(doc_1.count(i))
+    # # print(fre1)
+    # for i in doc_2:
+    #     fre2.append(doc_2.count(i))
+    # # print(fre2)
     fr_list = fre1+fre2
-    print(fr_list)
-    fr1_ = {}
-    fr2_ = {}
-    for i in doc_1:
-        if i not in fr1_:
-            fr1_[i] = doc_1.count(i) 
-    # print(fr1_)
-    for i in doc_2:
-        if i not in fr2_:
-            fr2_[i] = doc_2.count(i)
-    # print(fr2_)
-    num_ = 0
-    for i in fr1_.keys():
-        if i in fr2_.keys():
-            num_ = sum(fr1_[i] * fr2_[i])
-    print(num_)
+    # print(fr_list)
+    fr_dic = {}
+    for i in fr_list:
+        if i not in fr_dic:
+            fr_dic[i] = [doc_1.count(i), doc_2.count(i)]
+    print(fr_dic)
 
 
 
