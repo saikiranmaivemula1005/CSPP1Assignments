7c7dd0d992866548ba8b7f57559c9f399913719d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-17 18:19:52 +0530

    " -> 2 of 3 passed. pylint: 3.82/10 "

--- M16/CodeCampDocumentDistance/doc.py
+++ M16/CodeCampDocumentDistance/doc.py
@@ -8,10 +8,8 @@ def similarity(dict1, dict2):
         Compute the document distance as given in the PDF
     '''
     regex = re.compile('[^a-z]')
-
     words1 =[regex.sub("",w.strip()) for w in dict1.lower().split(" ")]
     words2 =[regex.sub("",w.strip()) for w in dict2.lower().split(" ")]
-
     dictionary = {}
     stopwords = load_stopwords("stopwords.txt")
     for w in words1:
@@ -19,23 +17,15 @@ def similarity(dict1, dict2):
             if w not in dictionary.keys():
                 dictionary[w]=[0,0]
             dictionary[w][0]+=1
-
-
-    for w in words2:
-        if w not in stopwords and len(w)>0:
-            if w not in dictionary.keys():
-                dictionary[w]=[0,0]
-            dictionary[w][1]+=1
-
-   
-
-
+    for word_ in words2:
+        if word_ not in stopwords and len(w)>0:
+            if word_ not in dictionary.keys():
+                dictionary[word_]=[0,0]
+            dictionary[word_][1]+=1
     num = sum([v1*v2 for v1,v2 in dictionary.values()])
-    den1 =  math.sqrt((sum([v1**2 for v1,v2 in dictionary.values()])))
-    den2 =  math.sqrt((sum([v2**2 for v1,v2 in dictionary.values()])))
-
+    den1 = math.sqrt((sum([v1**2 for v1,v2 in dictionary.values()])))
+    den2 = math.sqrt((sum([v2**2 for v1,v2 in dictionary.values()])))
     return num/(den1*den2)
-
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
@@ -45,15 +35,12 @@ def load_stopwords(filename):
         for line in filename:
             stopwords[line.strip()] = 0
     return stopwords
-
 def main():
     '''
         take two inputs and call the similarity function
     '''
     input1 = input()
     input2 = input()
-
     print(similarity(input1, input2))
-
 if __name__ == '__main__':
     main()
