e259acc301b61a664338f996ea00c902b34d1fb9
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-17 11:49:56 +0530

    " -> 0 of 3 passed. pylint: 8.16/10 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -32,8 +32,8 @@ def similarity(dict1, dict2):
     fr1_ = {}
     fr2_ = {}
     for i in doc_1:
-        doc_1[i] = fr1_[i][0]
-        fre1[i] = fr1_[i][1]
+        fr1_[i][0] = doc_1[i]
+        fr1_[i][1] = fre1[i]
     print(fr1)
 
 
