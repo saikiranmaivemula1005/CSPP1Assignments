73936cb8889bc145bc7851736e477a6fee679610
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-17 14:43:42 +0530

    " -> 2 of 3 passed. pylint: 9.44/10 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -16,6 +16,9 @@ def similarity(dict1, dict2):
     for i in doc_1:
         if i in ('!', '@', '#', '$', '%', '^', '&', '*', '?'):
             doc_1.remove(i)
+    for i in doc_2:
+        if i in ('!', '@', '#', '$', '%', '^', '&', '*', '?'):
+            doc_2.remove(i)
     # regex_ = re.compile([,^a-z])
     # doc_1 = [regex_.sub('', w) for w in doc_1]
     # doc_2 = [regex_.sub('', w) for w in doc_2]
