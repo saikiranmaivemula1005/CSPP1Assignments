9bf45d1b1985cf8956e94f9e1f023ea0c3d87fc9
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-17 11:20:57 +0530

    " -> 0 of 3 passed. pylint: 9.41/10 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -8,8 +8,8 @@ def similarity(dict1, dict2):
     '''
     doc_1 = str(dict1)
     doc_2 = str(dict2)
-    print(doc_1.lower().split(' '))
-    print(doc_2.lower().split(' '))
+    print(doc_1.lower().split(' ').remove(' '))
+    print(doc_2.lower().split(' ').remove(' '))
 
 def load_stopwords(filename):
     '''
