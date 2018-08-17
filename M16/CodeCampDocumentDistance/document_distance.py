d04d1c64658e760d99a7295dd66760545227048c
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-17 14:54:52 +0530

    " -> 2 of 3 passed. pylint: 9.23/10 "

--- M16/CodeCampDocumentDistance/document_distance.py
+++ M16/CodeCampDocumentDistance/document_distance.py
@@ -14,8 +14,8 @@ def similarity(dict1, dict2):
     doc_1 = doc_1.lower().split(' ')
     doc_2 = doc_2.lower().split(' ')
     regex_ = re.compile('^a-z')
-    doc_1 = [regex_.sub('', w) for w in doc_1]
-    doc_2 = [regex_.sub('', w) for w in doc_2]
+    doc_1 = [regex_.sub(' ', w) for w in doc_1]
+    doc_2 = [regex_.sub(' ', w) for w in doc_2]
     stopwords = load_stopwords("stopwords.txt")
     for i in doc_1:
         if i in stopwords:
