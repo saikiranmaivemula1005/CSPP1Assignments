52ff54c87ad25bf2dc987b7c8b370db2c534d2f0
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-18 12:20:47 +0530

    " -> 0 of 1 passed. pylint: 7.20/10 "

--- M18/p2/search.py
+++ M18/p2/search.py
@@ -41,7 +41,7 @@ def search(search_index, query):
     searchindex = {}
     # query = str(query)
     # query = query.split(' ')
-    # print(query)
+    print(query)
     # print(search_index)
     for i in query:
         if i not in searchindex.keys():
