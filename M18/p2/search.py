02a255fcc91cd628a6617d9fefd54262bd23bb2a
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-18 12:55:19 +0530

    " -> 0 of 1 passed. pylint: 5.91/10 "

--- M18/p2/search.py
+++ M18/p2/search.py
@@ -56,7 +56,7 @@ def search(search_index, query):
             # print(j)
             if j in search_index.keys():
                 # print(search_index)            
-                l1 = {l1 + search_index[j][0][0]}
+                l1 = {l1 + list(search_index[j][0][0])}
                 print(l1)
     return set(l1)
 
