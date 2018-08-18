b76351cfca770b20b7c3430d6e3cdf72f6d0e9c3
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-18 11:45:56 +0530

    " -> 0 of 1 passed. pylint: 7.39/10 "

--- M18/p1/build_search_index.py
+++ M18/p1/build_search_index.py
@@ -64,7 +64,7 @@ def build_search_index(docs):
     # return search index
     count = 0
     for i in docs:
-        print(i[1])
+        print(i)
         if i == '\n':
             count += 1
     print(count) 
