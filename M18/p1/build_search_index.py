bfa28469c9231b14d0522b33e93648211c33e893
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-18 17:36:16 +0530

    " -> 0 of 1 passed. pylint: 9.06/10 "

--- M18/p1/build_search_index.py
+++ M18/p1/build_search_index.py
@@ -53,6 +53,10 @@ def build_search_index(docs):
     # clean up doc and tokenize to words list
     # add or update the words of the doc to the search index
     # return search index
+    search_index = {}
+    for i in docs:
+        doc_id = docs.index(i)
+        print(doc_id)
     
 # helper function to print the search index
 # use this to verify how the search index looks
