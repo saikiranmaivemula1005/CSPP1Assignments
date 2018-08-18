aa41d716dccb6da7761abdbe5f2676112a345d74
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-18 12:07:14 +0530

    " -> 0 of 1 passed. pylint: 5.42/10 "

--- M18/p2/search.py
+++ M18/p2/search.py
@@ -38,6 +38,7 @@ def search(search_index, query):
         make a set of doc_id and return
     '''
     l1 = []
+    dictionary = {}
     # query = str(query)
     # query = query.split(' ')
     # print(query)
