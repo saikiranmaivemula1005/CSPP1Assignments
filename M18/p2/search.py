5b0ed3d8bd7ad55bd8372f706bcb6ccd74c93616
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-18 12:03:53 +0530

    " -> 0 of 1 passed. pylint: 7.39/10 "

--- M18/p2/search.py
+++ M18/p2/search.py
@@ -56,7 +56,7 @@ def process_queries(search_index, queries):
         print the results returned by search function
     '''
     for i in queries:
-        search(search_index, queries)
+        l1 = search(search_index, queries)
         print(l1)
 
 
