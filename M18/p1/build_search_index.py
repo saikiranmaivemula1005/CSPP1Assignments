7fe28ed910560fd6d172148c0ded2e503fcdea32
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-18 10:30:42 +0530

    " -> 0 of 1 passed. pylint: 7.18/10 "

--- M18/p1/build_search_index.py
+++ M18/p1/build_search_index.py
@@ -75,8 +75,8 @@ def build_search_index(docs):
                 wordslist.append(i)
     for i in wordslist:
         if i not in searchindex.keys():
-            searchindex[0].append(i)
-            searchindex[0]+=1
+            searchindex[1].append(i)
+            searchindex[1]+=1
     print(searchindex)
     
 
