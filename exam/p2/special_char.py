31afd6af6e116ac6e0f967cb71fe6638419a6c9d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-22 18:10:44 +0530

    " -> 7 of 7 passed. pylint: 9.09/10 "

--- exam/p2/special_char.py
+++ exam/p2/special_char.py
@@ -5,14 +5,14 @@ Output has three spaces, which are to be replaced with these special characters
 '''
 """string"""
 def main():
-    """special"""
+    """kk"""
     input_ = input()
     s1_ = ""
-    for i_ in input_:
-        if i_ in '!' '@' '#' '$' '%' '^' '&' '*':
+    for i1_ in input_:
+        if i1_ in '!' '@' '#' '$' '%' '^' '&' '*':
             s1_ = s1_ + " "
         else:
-            s1_ = s1_ + i_
+            s1_ = s1_ + i1_
     print(s1_)
 if __name__ == "__main__":
     main()
