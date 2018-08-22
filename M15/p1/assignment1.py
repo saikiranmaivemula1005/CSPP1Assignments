3f55955533d518e9257703f1bc9e78b158071b3f
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-22 17:32:07 +0530

    " -> 0 of 3 passed. pylint: 7.50/10 "

--- M15/p1/assignment1.py
+++ M15/p1/assignment1.py
@@ -259,6 +259,7 @@ class CiphertextMessage(Message):
                             m = m + x 
                 if m in self.valid_words:
                     if len(m) == len(message_text):
+                        print(i,m)
                         return i,m
 
 ### DO NOT MODIFY THIS METHOD ###
