5db5be0466f8c694e961277c51a7c4b323b1c67e
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-22 11:03:53 +0530

    " -> 5 of 5 passed. pylint: 9.23/10 "

--- M14/p2/assignment2.py
+++ M14/p2/assignment2.py
@@ -36,9 +36,7 @@ class Message(object):
     def __init__(self, text):
         '''
         Initializes a Message object
-
         text (string): the message's text
-
         a Message object has two attributes:
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words
@@ -50,7 +48,6 @@ class Message(object):
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
-
         Returns: self.message_text
         '''
         return self.message_text
@@ -58,7 +55,6 @@ class Message(object):
     def get_valid_words(self):
         '''
         Used to safely access a copy of self.valid_words outside of the class
-
         Returns: a COPY of self.valid_words
         '''
         return self.valid_words[:]
@@ -69,10 +65,8 @@ class Message(object):
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
         letters only.
-
         shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
-
         Returns: a dictionary mapping a letter (string) to
                  another letter (string).
         '''
@@ -91,10 +85,8 @@ class Message(object):
         Applies the Caesar Cipher to self.message_text with the input shift.
         Creates a new string that is self.message_text shifted down the
         alphabet by some number of characters determined by the input shift
-
         shift (integer): the shift with which to encrypt the message.
         0 <= shift < 26
-
         Returns: the message text (string) in which every character is shifted
              down the alphabet by the input shift
         '''
@@ -111,19 +103,16 @@ class Message(object):
 class PlaintextMessage(Message):
     def __init__(self, text, shift):
         '''
-        Initializes a PlaintextMessage object        
-        
+        Initializes a PlaintextMessage object
         text (string): the message's text
         shift (integer): the shift associated with this message
-
         A PlaintextMessage object inherits from Message and has five attributes:
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
             self.shift (integer, determined by input shift)
             self.encrypting_dict (dictionary, built using shift)
             self.message_text_encrypted (string, created using shift)
-
-        Hint: consider using the parent class constructor so less 
+        Hint: consider using the parent class constructor so less
         code is repeated
         '''
         self.message_text = text
@@ -134,21 +123,18 @@ class PlaintextMessage(Message):
     def get_shift(self):
         '''
         Used to safely access self.shift outside of the class
-        
         Returns: self.shift
         '''
         return self.shift
     def get_encrypting_dict(self):
         '''
         Used to safely access a copy self.encrypting_dict outside of the class
-        
         Returns: a COPY of self.encrypting_dict
         '''
         return self.encrypting_dict
     def get_message_text_encrypted(self):
         '''
         Used to safely access self.message_text_encrypted outside of the class
-        
         Returns: self.message_text_encrypted
         '''
         return self.message_text_encrypted
@@ -157,10 +143,8 @@ class PlaintextMessage(Message):
         Changes self.shift of the PlaintextMessage and updates other 
         attributes determined by shift (ie. self.encrypting_dict and 
         message_text_encrypted).
-        
         shift (integer): the new shift that should be associated with this message.
         0 <= shift < 26
-
         Returns: nothing
         '''
         self.shift = shift
@@ -179,6 +163,5 @@ def main():
     print(data.get_shift())
     print(data.get_encrypting_dict())
     print(data.get_message_text_encrypted())
-
 if __name__ == "__main__":
     main()
