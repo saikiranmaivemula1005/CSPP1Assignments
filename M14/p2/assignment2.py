1f28d56dbac97a1002f1e293f3b87d0c5fb7a954
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-22 10:41:06 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- M14/p2/assignment2.py
+++ M14/p2/assignment2.py
@@ -152,7 +152,7 @@ class PlaintextMessage(Message):
         self.message_text = text
         self.valid_words = load_words(WORDLIST_FILENAME)
         self.shift = shift
-        self.build_shift_dict(self,shift) = encrypting_dict
+        build_shift_dict(self,shift) = encrypting_dict
         
 
     def get_shift(self):
