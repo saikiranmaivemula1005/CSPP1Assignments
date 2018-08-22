ff1d2cb18bf168bc9be94a148c7894bc9c6076ca
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-22 14:23:12 +0530

    " -> 0 of 3 passed. pylint: 9.05/10 "

--- M15/p1/assignment1.py
+++ M15/p1/assignment1.py
@@ -229,9 +229,8 @@ class CiphertextMessage(Message):
             self.valid_words (list, determined using helper function load_words)
         '''
         self.message_text = text
+        print(self.message_text)
         self.valid_words = load_words("words.txt")
-        print(self.valid_words)
-
     def decrypt_message(self):
         '''
         Decrypt self.message_text by trying every possible shift value
@@ -240,15 +239,13 @@ class CiphertextMessage(Message):
         on the message text. If s is the original shift value used to encrypt
         the message, then we would expect 26 - s to be the best shift value
         for decrypting it.
-
         Note: if multiple shifts are  equally good such that they all create
         the maximum number of you may choose any of those shifts (and their
         corresponding decrypted messages) to return
-
         Returns: a tuple of the best shift value used to decrypt the message
         and the decrypted message text using that shift value
         '''
-        pass
+        
 
 
 ### DO NOT MODIFY THIS METHOD ###
