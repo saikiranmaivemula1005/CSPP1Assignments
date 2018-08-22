9d4087cf262168132225312ba299e888f49d6c76
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-22 10:43:36 +0530

    " -> 0 of 5 passed. pylint: 4.38/10 "

--- M14/p2/assignment2.py
+++ M14/p2/assignment2.py
@@ -152,7 +152,7 @@ class PlaintextMessage(Message):
         self.message_text = text
         self.valid_words = load_words(WORDLIST_FILENAME)
         self.shift = shift
-        print(build_shift_dict(self,shift))
+        print(Message.build_shift_dict(self,shift))
         
 
     def get_shift(self):
