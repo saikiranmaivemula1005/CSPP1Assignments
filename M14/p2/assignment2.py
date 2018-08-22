8b74832412bc5a95047a9d85587b74aa69da5430
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-22 11:01:16 +0530

    " -> 5 of 5 passed. pylint: 8.15/10 "

--- M14/p2/assignment2.py
+++ M14/p2/assignment2.py
@@ -1,11 +1,9 @@
 ''' Problem 2 - PlaintextMessage '''
 # For this problem, the graders will use our implementation of the Message class,
 # so don't worry if you did not get the previous parts correct.
-
 # PlaintextMessage is a subclass of Message and has methods to encode a string
 # using a specified shift value. Our class will always create an encoded version
 # of the message, and will have methods for changing the encoding.
-
 # Implement the methods in the class PlaintextMessage according to the specifications in ps6.py.
 # The methods you should fill in are:
 # __init__(self, text, shift): Use the parent class constructor to make your code more concise.
@@ -13,22 +11,16 @@
 # The getter method get_encrypting_dict(self): This should return a COPY of self.encrypting_dict.
 # The getter method get_message_text_encrypted(self)
 # change_shift(self, shift): Think about what other methods you can use to make this easier.
-
 ### Helper code
-
 import string
-
 def load_words(file_name):
     '''
     file_name (string): the name of the file containing
     the list of words to load
-
     Returns: a list of valid words. Words are strings of lowercase letters.
-
     Depending on the size of the word list, this function may
     take a while to finish.
     '''
-
     # inFile: file
     in_file = open(file_name, 'r')
     # line: string
@@ -37,12 +29,9 @@ def load_words(file_name):
     word_list = line.split()
     in_file.close()
     return word_list
-
 WORDLIST_FILENAME = 'words.txt'
-
 class Message(object):
     ''' Grader's Implementation of Message Object '''
-
     ### DO NOT MODIFY THIS METHOD ###
     def __init__(self, text):
         '''
@@ -57,7 +46,6 @@ class Message(object):
         self.message_text = text
         self.valid_words = load_words("words.txt")
         self.shift_dict = {}
-
     ### DO NOT MODIFY THIS METHOD ###
     def get_message_text(self):
         '''
@@ -66,7 +54,6 @@ class Message(object):
         Returns: self.message_text
         '''
         return self.message_text
-
     ### DO NOT MODIFY THIS METHOD ###
     def get_valid_words(self):
         '''
@@ -75,7 +62,6 @@ class Message(object):
         Returns: a COPY of self.valid_words
         '''
         return self.valid_words[:]
-
     def build_shift_dict(self, shift):
         '''
         Creates a dictionary that can be used to apply a cipher to a letter.
@@ -93,18 +79,13 @@ class Message(object):
         lower_keys = list(string.ascii_lowercase)
         lower_values = list(string.ascii_lowercase)
         shift_lower_values = lower_values[shift:] + lower_values[:shift]
-
         upper_keys = list(string.ascii_uppercase)
         upper_values = list(string.ascii_uppercase)
         upper_shift_values = upper_values[shift:] + upper_values[:shift]
-
         full_keys = lower_keys + upper_keys
         full_values = shift_lower_values + upper_shift_values
-
         self.shift_dict = dict(zip(full_keys, full_values))
         return self.shift_dict
-
-
     def apply_shift(self, shift):
         '''
         Applies the Caesar Cipher to self.message_text with the input shift.
@@ -125,11 +106,7 @@ class Message(object):
             else:
                 new_msg.append(self.build_shift_dict(shift)[i])
         return ''.join(new_msg)
-
 ### Helper code End
-
-
-
 ### Paste your implementation of the `PlaintextMessage` class here
 class PlaintextMessage(Message):
     def __init__(self, text, shift):
@@ -152,10 +129,8 @@ class PlaintextMessage(Message):
         self.message_text = text
         self.valid_words = load_words(WORDLIST_FILENAME)
         self.shift = shift
-        self.encrypting_dict = Message.build_shift_dict(self,shift)
-        self.message_text_encrypted = Message.apply_shift(self,shift)
-        
-
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
     def get_shift(self):
         '''
         Used to safely access self.shift outside of the class
@@ -163,7 +138,6 @@ class PlaintextMessage(Message):
         Returns: self.shift
         '''
         return self.shift
-
     def get_encrypting_dict(self):
         '''
         Used to safely access a copy self.encrypting_dict outside of the class
@@ -178,7 +152,6 @@ class PlaintextMessage(Message):
         Returns: self.message_text_encrypted
         '''
         return self.message_text_encrypted
-
     def change_shift(self, shift):
         '''
         Changes self.shift of the PlaintextMessage and updates other 
@@ -193,12 +166,8 @@ class PlaintextMessage(Message):
         self.shift = shift
         # Message.build_shift_dict(self,shift)
         # Message.apply_shift(self,shift)
-        self.encrypting_dict = Message.build_shift_dict(self,shift)
-        self.message_text_encrypted = Message.apply_shift(self,shift)
-
-
-
-
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
 def main():
     ''' Function to handle testcases '''
     inp = input()
