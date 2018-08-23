e5a12690182d1768577c76a62de483f52050c438
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-23 16:12:14 +0530

    " -> 3 of 3 passed. pylint: 9.41/10 "

--- M15/p2/assignment2.py
+++ M15/p2/assignment2.py
@@ -264,7 +264,7 @@ def decrypt_story():
     ''' Decrypt the story text using CiphertextMessage class and return the
         shift value and decrypted string in a tuple.
     '''
-    pass #delete this line when you write your code.
+    
 
 ### DO NOT MODIFY THIS METHOD ###
 def main():
