27b7f313bbf1d6d1d3f1662ee2fefbe01f1cb7b0
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-24 14:41:59 +0530

    " -> 0 of 14 passed. pylint: 5.56/10 "

--- M21/tac.py
+++ M21/tac.py
@@ -1,26 +1,10 @@
-rows = 3
-matrix = []
-lst = []
-c = 0
-for i1_ in range(rows):
-    l1_ = [i1_ for i1_ in input().split(' ')]
-    matrix.append(l1_)
-# print(matrix)
-for i in range(len(matrix)):
-    if i not in ('x','o','.'):
-        # print(j)
-        c += 1
-    # if c >= 1:
-    #     print('invalid input')
-    #     exit()
-for i in range(len(matrix)):
-    # print(i)
-    # print(matrix.count(matrix[i]))
-    if i.count('o') == 3:
-        print('o')
-    if i.count('x') == 3:
-        print('x')
-    # for j in range(len(matrix)):
-    #     # print(matrix[i][j])
-    #     print(matrix.count(matrix[i][j]))
-    #         # print(matrix[i][j])
+def main():
+    matrix = []
+    i = 0
+    while i < 3:
+        l1 = input().split(' ')
+        matrix.append(l1)
+        i += 1
+    print(matrix)
+main()
+
