65edca3a13f85385f605203ba716e20612e92e25
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-24 10:20:28 +0530

    " -> 13 of 14 passed. pylint: 6.25/10 "

--- M21/tic.py
+++ M21/tic.py
@@ -15,12 +15,12 @@ for i in matrix:
 if c >= 1:
     print('invalid input')
     exit()
-print(lst)
+# print(lst)
 
 if lst.count('x') == 3 and lst.count('o') == 3 and lst.count('.') != 3:
     print('draw')
 if lst.count('x') == lst.count('o'):
-    print('draw')
+    print('invalid game')
 elif lst.count('x') == 3 and lst.count('o') < 3:
     print('x')
 elif lst.count('o') == 3 and lst.count('x') < 3:
