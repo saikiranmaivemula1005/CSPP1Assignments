931d7b314db2dd2184d1a97a799ab3f83f0b5a07
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-24 11:40:09 +0530

    " -> 0 of 14 passed. pylint: 4.38/10 "

--- M21/tac.py
+++ M21/tac.py
@@ -9,7 +9,7 @@ for i1_ in range(rows):
 for i in matrix:
     for j in i:
         lst.append(j)
-        print(j)
+        print(matrix[i][j])
     if j not in ('x','o','.'):
         # print(j)
         c += 1
