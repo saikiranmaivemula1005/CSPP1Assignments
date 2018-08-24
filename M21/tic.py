5f9db03314a428db74afd1bee20ed96a195e6bf9
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-24 09:31:47 +0530

    " -> 0 of 14 passed. pylint: 1.11/10 "

--- M21/tic.py
+++ M21/tic.py
@@ -3,4 +3,9 @@ matrix = []
 for i1_ in range(rows):
     l1_ = [i1_ for i1_ in input().split(' ')]
     matrix.append(l1_)
-print(matrix)
+# print(matrix)
+for i in range(len(rows)):
+	if matrix.count(i) == 3:
+		print(matrix[i])
+	else:
+		print('invalid game')
