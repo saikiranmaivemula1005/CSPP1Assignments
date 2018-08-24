1cc8f6f6f905766ffde755c44ca7b2184669083c
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-24 10:01:33 +0530

    " -> 0 of 14 passed. pylint: 2.94/10 "

--- M21/tic.py
+++ M21/tic.py
@@ -4,7 +4,7 @@ lst = []
 for i1_ in range(rows):
     l1_ = [i1_ for i1_ in input().split(' ')]
     matrix.append(l1_)
-print(matrix)
+# print(matrix)
 for i in matrix:
 	for j in i:
 		lst.append(j)
