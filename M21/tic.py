e45f6e56d1cf5eae0f0f465bb157d6d8767ad19a
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-24 10:15:41 +0530

    " -> 0 of 14 passed. pylint: 3.18/10 "

--- M21/tic.py
+++ M21/tic.py
@@ -3,25 +3,25 @@ matrix = []
 lst = []
 c = 0
 for i1_ in range(rows):
-	l1_ = [i1_ for i1_ in input().split(' ')]
-	matrix.append(l1_)
+    l1_ = [i1_ for i1_ in input().split(' ')]
+    matrix.append(l1_)
 # print(matrix)
 for i in matrix:
-	for j in i:
-		lst.append(j)
-	if j not in ('x','o','.'):
-		# print(j)
-		c += 1
+    for j in i:
+        lst.append(j)
+    if j not in ('x','o','.'):
+        # print(j)
+        c += 1
 if c >= 1:
-	print('invalid input')
-	break
+    print('invalid input')
+    break
 # print(lst)
 
 if lst.count('x') == 3 and lst.count('o') == 3 and lst.count('.') != 3 or lst.count('x') == lst.count('o'):
-	print('draw')
+    print('draw')
 elif lst.count('x') == 3 and lst.count('o') < 3:
-	print('x')
+    print('x')
 elif lst.count('o') == 3 and lst.count('x') < 3:
-	print('o')
+    print('o')
 else:
-	print('invalid game')
+    print('invalid game')
