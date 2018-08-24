debc9bbe0055fb2b8ad87945bf4a2949e1406ae7
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-24 10:01:07 +0530

    " -> 0 of 14 passed. pylint: 3.33/10 "

--- M21/tic.py
+++ M21/tic.py
@@ -4,11 +4,11 @@ lst = []
 for i1_ in range(rows):
     l1_ = [i1_ for i1_ in input().split(' ')]
     matrix.append(l1_)
-# print(matrix)
+print(matrix)
 for i in matrix:
 	for j in i:
 		lst.append(j)
-# print(lst)
+print(lst)
 if lst.count('x') == 3 and lst.count('o') == 3 and lst.count('.') != 3 and lst.count('x') == lst.count('o'):
 	print('draw')
 elif lst.count('x') == 3:
