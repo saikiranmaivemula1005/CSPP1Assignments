103fd1cbd702a59b6f9a9582c0461905840e0f44
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-25 17:22:24 +0530

    " -> 0 of 26 passed. pylint: 0/0 "

--- M22/Check Sudoku/check_sudoku.py
+++ M22/Check Sudoku/check_sudoku.py
@@ -14,10 +14,10 @@ def check_sudoku(sudoku):
         The function has to return True for a valid sudoku grid and false otherwise
     '''
     # print(sudoku)
-    for i in range(len(sudoku)):
-       # print(i)
     l1 = []
     l2 = []
+    for i in range(len(sudoku)):
+       # print(i)
     for j in range(len(sudoku)):
         if sudoku[i][j] in l1:
             return False
