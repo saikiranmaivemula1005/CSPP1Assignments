9bdc4c990d01b7a77ef62775ad76d6ef3bfbb8f1
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-25 11:32:22 +0530

    " -> 0 of 3 passed. pylint: 2.50/10 "

--- M22/assignment4/print_dictionary.py
+++ M22/assignment4/print_dictionary.py
@@ -5,11 +5,14 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
+	for i in dictionary:
+		print(i)
+
     
 
 def main():
     dictionary = eval(input())
-    print(dictionary)
+    # print(dictionary)
     print_dictionary(dictionary)
 
 if __name__ == '__main__':
