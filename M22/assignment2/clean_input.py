55d1c75ee3820c1441bf4bb66637b87c35abdc54
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-25 10:46:25 +0530

    " -> 3 of 3 passed. pylint: 9.09/10 "

--- M22/assignment2/clean_input.py
+++ M22/assignment2/clean_input.py
@@ -2,13 +2,13 @@
 Write a function to clean up a given string by removing the special characters and retain
 alphabets in both upper and lower case and numbers.
 '''
-l1_ = []
+lst_ = []
 def clean_string(string):
     """kk"""
     for i1_ in string:
         if i1_ not in '!@#$%^&*() .':
-            l1_.append(i1_)
-    return ''.join(l1_)
+            lst_.append(i1_)
+    return ''.join(lst_)
 
 
 def main():
