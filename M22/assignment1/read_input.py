f7e4b8844acff5116fcfa7afd6303914ac77589f
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-25 10:39:00 +0530

    " -> 0 of 2 passed. pylint: 8.89/10 "

--- M22/assignment1/read_input.py
+++ M22/assignment1/read_input.py
@@ -8,6 +8,6 @@ def main():
     for i in range(lines):
         documents.append(input())
         i += 1
-    print(documents)
+    print(''.join(documents))
 if __name__ == '__main__':
     main()
