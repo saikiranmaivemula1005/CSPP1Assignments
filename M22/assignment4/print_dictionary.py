0da4c00221a292c8bcfc95e9a26654d5d3c1a560
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-25 11:29:50 +0530

    " -> 0 of 4 passed. pylint: 6.67/10 "

--- M22/assignment4/print_dictionary.py
+++ M22/assignment4/print_dictionary.py
@@ -4,13 +4,13 @@ frequency of each word. Format of the printing should be one key per line and se
 the key and frequency with a SPACE - SPACE.
 '''
 
-# def print_dictionary(dictionary):
+def print_dictionary(dictionary):
     
 
 def main():
     dictionary = eval(input())
     print(dictionary)
-    # print_dictionary(dictionary)
+    print_dictionary(dictionary)
 
 if __name__ == '__main__':
     main()
