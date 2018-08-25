ed85e1f826667bd3abcae63789d74ac27f5de038
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-08-25 12:05:55 +0530

    " -> 2 of 3 passed. pylint: 9.29/10 "

--- M22/assignment4/print_dictionary.py
+++ M22/assignment4/print_dictionary.py
@@ -3,26 +3,22 @@ Write a function to print a dictionary with the keys in sorted order along with 
 frequency of each word. Format of the printing should be one key per line and separate
 the key and frequency with a SPACE - SPACE.
 '''
-l1 = []
-l2 = []
+LIST1 = []
+LIST2 = []
 def print_dictionary(dictionary):
-	for i in dictionary.keys():
-		l1.append(i)
-		l2.append(dictionary[i])
-	# print(sorted(l1))
-	l3 = sorted(l1)
-	# print(l 2)
-	for i in range(len(l1)):
-		print(str(l3[i])+' '+'-'+ ' '+ str(l2[i]))
-
-
-
-    
-
+    """kk"""
+    for i1_ in dictionary.keys():
+        LIST1.append(i1_)
+        LIST2.append(dictionary[i1_])
+    # print(sorted(l1))
+    l3_ = sorted(LIST1)
+    # print(l 2)
+    for i1_ in range(len(LIST1)):
+        print(str(l3_[i1_])+' '+'-'+ ' '+ str(LIST2[i1_]))
 def main():
+    """kk"""
     dictionary = eval(input())
     # print(dictionary)
     print_dictionary(dictionary)
-
 if __name__ == '__main__':
     main()
